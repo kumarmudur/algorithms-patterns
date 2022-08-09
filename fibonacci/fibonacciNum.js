2df416b9a422fb33ab9b40b6e0ef994bd2d9ff01
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-08-09 05:36:23 +0530

    [Fibonacci Pattern] fibonacci number pateern with memoization

--- fibonacci/fibonacciNum.js
+++ fibonacci/fibonacciNum.js
@@ -3,10 +3,28 @@
 // Mathematically define fibonacci numbers
 // Fib(n) = Fib(n-1) + Fib(n-2), for n > 1
 
-// Solution 1: Recursion
+// Solution 1: Recursion time: O(2^n) | space: O(n)
 const calculateFibonacci = (n) => {
+    console.log('fib called', n);
     if (n < 2) return n;
     return calculateFibonacci(n - 1) + calculateFibonacci(n - 2);
 };
+
+// Solution 2: Top-down dynamic programming with memoization 
+const calculateFibonacci1 = (n) => {
+    const memoize = [];
+
+    function fib(n) {
+        if (n < 2) return n;
+
+        // If have already solved this subprogram, simply return the result from the cache 
+        if (memoize[n]) return memoize[n];
+
+        memoize[n] = fib(n-1) + fib(n-2);
+        return memoize[n];
+    }
+
+    return fib(n);
+}
 
 console.log(calculateFibonacci(10));
