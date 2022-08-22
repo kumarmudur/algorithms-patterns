7154bee3d45da4ce9c424c74c430d11fc908a20c
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-08-22 23:02:46 +0530

    [JAVA][Two Pointers] pair with target sum example with hash table solution

--- twoPointers/targetSum.java
+++ twoPointers/targetSum.java
@@ -1,5 +1,7 @@
 package twoPointers;
 
+import java.util.HashMap;
+
 // time: O(n) | space: O(1) 
 class PairWithTargetSum {
     public static int[] search(int[] arr, int targetSum) {
@@ -14,6 +16,23 @@ class PairWithTargetSum {
                 left++;
             else
                 right--;
+        }
+        return new int[] { -1, -1 };
+    }
+};
+
+// Solution 2
+// Hash Table
+// time: O(n) | space: O(n)
+class PairWithTargetSumOne {
+    public static int[] search(int[] arr, int targetSum) {
+        HashMap<Integer, Integer> results = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (results.containsKey(targetSum - arr[i]))
+                return new int[] { results.get(targetSum - arr[i]), i };
+            else
+                results.put(arr[i], i);
         }
         return new int[] { -1, -1 };
     }
