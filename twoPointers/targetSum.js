3dc1e905ceb6fe1f332b46a86271e7411b4a9ef7
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-08-22 22:54:07 +0530

    [JS][Two Pointers] pair with target sum example with hash table solution

--- twoPointers/targetSum.js
+++ twoPointers/targetSum.js
@@ -1,5 +1,5 @@
+// Solution 1
 // time: O(n) | space O(1)
-
 function pair_with_target_sum(arr, targetSum) {
     let left = 0;
     let right = arr.length - 1;
@@ -16,3 +16,20 @@ function pair_with_target_sum(arr, targetSum) {
     }
     return [-1, -1];
 }
+
+// Solution 2
+// Hash Table
+// time: O(n) | space O(n)
+function pair_with_target_sum(arr, targetSum) {
+    const results = {};
+
+    for (let i = 0; i < arr.length; i++) {
+        const num = arr[i];
+        if (targetSum - num in results) {
+            return [results[targetSum - num], i];
+        }
+        results[arr[i]] = i;
+    }
+    return [ -1, -1];
+}
+
