1e8289e9009a834b8ff108945120f7e9b7d8d552
kumarmudur <kumarmudur10@gmail.com>
2023-12-02 22:02:54 -0500

    [Two Pointer] not duplicate count example

---
+++ twoPointers/nonDuplicate.js
@@ -0,0 +1,18 @@
+// Input: [2, 3, 3, 3, 6, 9, 9]
+// Output: 4
+
+const remove = (arr) => {
+    let nextNonDuplicate = 1;
+    let i = 0;
+
+    while (i < arr.length) {
+        if (arr[nextNonDuplicate - 1] !== arr[i]) {
+            arr[nextNonDuplicate] = arr[i];
+            nextNonDuplicate++;
+        }
+        i++;
+    }
+    return nextNonDuplicate;
+};
+
+console.log(remove([2, 3, 3, 3, 6, 9, 9]));
