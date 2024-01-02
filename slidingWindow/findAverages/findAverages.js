241353081459f22c86a975afa69c8cc8f8addbb4
kumarmudur <kumarmudur10@gmail.com>
2024-01-02 18:11:58 -0500

    [SlidingWindow] find averages sum example

---
+++ slidingWindow/findAverages/findAverages.js
@@ -0,0 +1,16 @@
+// Array: [1, 3, 2, 6, -1, 4, 1, 8, 2], K=5, output = [2.2, 2.8, 2.4, 3.6, 2.8]
+
+// time: O(N ^ K) | space: O(N)
+const findAverages = (arr, k) => {
+    const result = [];
+
+    for (let i = 0; i < arr.length - k + 1; i++) {
+        let sum = 0;
+        for (let j = 0; j < i + k; j++) {
+            sum += arr[j];
+        }
+        result.push(sum / k); // calculate average
+    }
+    return result;
+}
+
